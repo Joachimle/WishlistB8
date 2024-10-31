f56162731820a026e7fb01c28393557e4b320b76
Aluna0001 <lusz0001@stud.kea.dk>
2024-10-31 14:40:50 +0100

    Test UserProfileController.

---
+++ src/test/java/beight/wishlist/controller/UserProfileControllerTests.java
@@ -0,0 +1,45 @@
+package beight.wishlist.controller;
+
+import beight.wishlist.model.UserProfile;
+import beight.wishlist.service.UserProfileService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@WebMvcTest(UserProfileController.class)
+class UserProfileControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private UserProfileService userProfileService;
+
+    @Test
+    void frontPage() throws Exception {
+        mockMvc.perform(get("/"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("frontpage"));
+    }
+
+    @Test
+    void createUser() throws Exception {
+        mockMvc.perform(get("/create_user"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("create_user"));
+    }
+
+    @Test
+    void saveUser() throws Exception {
+        mockMvc.perform(post("/save_user").sessionAttr("userProfile", new UserProfile()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name("redirect:/"));
+    }
+
+}
