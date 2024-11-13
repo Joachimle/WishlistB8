03dd5d8060c4b5d162fd1a00668a9118f7596adc
zako0001 <zako0001@stud.kea.dk>
2024-11-13 11:29:55 +0100

    h2 seems to work

--- src/main/resources/wishlist_data.sql
+++
@@ -1,8 +0,0 @@
-INSERT INTO wishes(wishName,price,wishDecription,link) VALUES('Iphone','8900','En ny telefon','applestore.com');
-INSERT INTO wishes(wishName,price,wishDecription,link) VALUES('Hund','100','Et kæledyr','nyhvalp.dk');
-
-INSERT INTO users (userName,userPassword) VALUES ('testemail@yahoo.dk','teskode');
-INSERT INTO users (userName,userPassword) VALUES ('testemail@gmail.dk','kodetest');
-
-INSERT INTO wishlist(wishlistName) VALUES ('juleønsker');
-INSERT INTO wishlist(wishlistName) VALUES ('fødselsdagsønsker');
